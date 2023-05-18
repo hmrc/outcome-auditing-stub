d0afa99951ff12c4e08e7a8b4faf4d3450f285f3
hmrc-web-operations <hmrc-web-operations@digital.hmrc.gov.uk>
2023-05-18 10:23:48 +0000

    'Creating new service outcome-auditing-stub'

---
+++ app/uk/gov/hmrc/outcomeauditingstub/config/Module.scala
@@ -0,0 +1,11 @@
+package uk.gov.hmrc.outcomeauditingstub.config
+
+import com.google.inject.AbstractModule
+
+class Module extends AbstractModule {
+
+  override def configure(): Unit = {
+
+    bind(classOf[AppConfig]).asEagerSingleton()
+  }
+}
